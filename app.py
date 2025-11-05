2a9e40a5a889e4ccc630cd684ed0e678e52d0101
Sathish <sathishveera004@gmail.com>
2025-11-05 12:15:00 +0530

    Prepare for Vercel deployment

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-app=Flask(__name__)
-
-
-app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///todo.db"
-db=SQLAlchemy(app )
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+db = SQLAlchemy(app)
 
 class task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,15 +12,18 @@ class task(db.Model):
     complete = db.Column(db.Integer, default=0)
     created = db.Column(db.DateTime, default=datetime.utcnow)
 
-
     def __repr__(self):
         return f"Task {self.id}"
 
-@app.route("/",methods=["GET","POST"])
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method=="POST":
-        current_task=request.form["content"]
-        mytask=task(content=current_task)
+    if request.method == "POST":
+        current_task = request.form["content"]
+        mytask = task(content=current_task)
 
         try:
             db.session.add(mytask)
@@ -31,12 +32,12 @@ def index():
         except Exception as e:
             return f"ERROR {e}"
     else:
-        mytask=task.query.order_by(task.created).all()
-        return render_template("index.html",tasks=mytask)
+        mytask = task.query.order_by(task.created).all()
+        return render_template("index.html", tasks=mytask)
 
 @app.route("/delete/<int:id>")
-def delete(id:int):
-    delete_task=task.query.get_or_404(id)
+def delete(id: int):
+    delete_task = task.query.get_or_404(id)
     try:
         db.session.delete(delete_task)
         db.session.commit()
@@ -44,23 +45,16 @@ def delete(id:int):
     except Exception as e:
         return f"ERROR {e}"
 
-@app.route("/update/<int:id>",methods=["GET","POST"])
-def update(id:int):
-    update_task=task.query.get_or_404(id)
-    if request.method=="POST":
-        update_task.content=request.form['content']
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id: int):
+    update_task = task.query.get_or_404(id)
+    if request.method == "POST":
+        update_task.content = request.form['content']
         try:
             db.session.commit()
             return redirect("/")
         except Exception as e:
             return f"ERROR {e}"
     else:
-        return render_template("update.html",task=update_task)
+        return render_template("update.html", task=update_task)
 
-
-if __name__ ==  "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
-
-    
